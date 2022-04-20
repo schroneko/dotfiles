3f4fafc213baf7cab4671794b5b795017f5782cc
schroneko <schroneko.fuller@gmail.com>
2022-04-21 00:16:41 +0900

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 set -eux
-EXEPATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
+# EXEPATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
+
+## ========== Clone Repo ==========
+if [ ! -d ~/dotfiles ]; then
+  git clone git@github.com:schroneko/dotfiles.git
+fi
+
+## ========== Install Homebrew ==========
+if [ ! -f /opt/homebrew/bin/brew ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 ## ========== Brew Bundle ==========
 brew upgrade
