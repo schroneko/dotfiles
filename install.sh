821074cecbd6572ba3655054e805f7a767206c0c
schroneko <schroneko.fuller@gmail.com>
2022-04-22 00:34:51 +0900

    update

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#! /bin/zsh
 set -eux
 
 ## ========== Dotfiles Path ==========
 DOT_DIR=$HOME/dotfiles
 
 ## ========== Not macOS ==========
-if [ "$(uname)" != "Darwin" ] ; then
+if [ "$(uname)" != "Darwin" ]; then
 	echo "This Scripts is for macOS!"
 	exit 1
 fi
@@ -16,26 +16,23 @@ if [ ! -d $DOT_DIR ]; then
 fi
 
 ## ========== Create Symbolic Links ==========
-if if [ ! -d ${DOT_DIR} ]; then
-  cd $DOT_DIR
-  for dotfile in .*; do
-    [[ "$dotfile" == ".git" ]] && continue
-    [[ "$dotfile" == ".DS_Store" ]] && continue
-    ln -snfv $DOT_DIR/"$dotfile" $HOME/"$dotfile"
-  done
-fi
+# if if [ ! -d ${DOT_DIR} ]; then
+#   cd $DOT_DIR
+#   for dotfile in .*; do
+#     [[ "$dotfile" == ".git" ]] && continue
+#     [[ "$dotfile" == ".DS_Store" ]] && continue
+#     ln -snfv $DOT_DIR/"$dotfile" $HOME/"$dotfile"
+#   done
+# fi
 
 ## ========== Xcode ==========
 # xcode-select --install >/dev/null 2>&1
 
 ## ========== Homebrew ==========
-which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 ## ========== Source zshrc ==========
 source $HOME/.zshrc
 
 ## ========== Brew Bundle ==========
 brew bundle --global
-brew update
-brew upgrade
-
