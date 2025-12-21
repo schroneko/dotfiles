5436029660796b55d0ad1c2079998f09c6f0fd62
schroneko <schroneko@users.noreply.github.com>
2025-12-22 05:34:46 +0900

    nvim 設定を追加、starship を削除、iina を追加
    starship は使わなくなったため管理対象から外した

---
+++ dot_config/nvim/private_init.lua
@@ -0,0 +1,69 @@
+-- 既存の .vimrc を読み込む
+vim.cmd('source ~/.vimrc')
+
+-- lazy.nvim のブートストラップ
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- プラグイン設定
+require('lazy').setup({
+  -- Tree-sitter: AST ベースのシンタックスハイライト
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          'typescript',
+          'javascript',
+          'tsx',
+          'json',
+          'yaml',
+          'markdown',
+          'markdown_inline',
+          'lua',
+          'vim',
+          'vimdoc',
+        },
+        highlight = {
+          enable = true,
+          -- 従来の正規表現ベースのハイライトを無効化
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+})
+
+-- Tree-sitter が有効な場合は termguicolors を有効化
+vim.opt.termguicolors = true
+
+-- 背景を透明にする（Ghostty の半透明背景を活かす）
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'NONE', ctermbg = 'NONE' })
+  end,
+})
+
+-- 初回読み込み時にも適用
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'NONE', ctermbg = 'NONE' })
